9f0c8c720ffd3e69516c3702e7ab51b59de811bf
danielfisher3 <38573896+danielfisher3@users.noreply.github.com>
2022-12-03 18:50:26 -0600

    Added DE and Right and animations/Start input

--- MiniBR/Assets/_MyAssets/Scripts/User/HandAnimationManager.cs
+++ MiniBR/Assets/_MyAssets/Scripts/User/HandAnimationManager.cs
@@ -1,18 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class HandAnimationManager : MonoBehaviour
 {
+    [SerializeField] InputActionReference gripRef;
+    [SerializeField]InputActionReference triggerRef;
+    
+    public float speed;
+    Animator hAnimator;
+    float gripTarget;
+    float triggerTarget;
+    float gripCurrent;
+    float triggerCurrent;
+
+    string gripP = "Grip";
+    string triggerP = "Trigger";
+
+    [SerializeField] bool hasGun = false;
+    [SerializeField] GameObject handgun;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        hAnimator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        SetGrip(gripRef.action.ReadValue<float>());
+        SetTrigger(triggerRef.action.ReadValue<float>());
+
+        if (!hasGun)
+        {
+            handgun.SetActive(false);
+        }
+        else if(hasGun && gripRef.action.ReadValue<float>() >= 0.25f)
+        {
+            handgun.SetActive(true);
+        }
+        else if(hasGun && gripRef.action.ReadValue<float>() < 0.25f)
+        {
+            handgun.SetActive(false);
+        }
+
+        AnimateHand();
+    }
+
+    internal void SetTrigger(float v)
+    {
+        triggerTarget = v;
+    }
+
+    internal void SetGrip(float v)
+    {
+        gripTarget = v;
+    }
+
+    void AnimateHand()
+    {
+        hAnimator.SetBool("GunInHand", hasGun);
+        if(gripCurrent != gripTarget)
+        {
+            gripCurrent = Mathf.MoveTowards(gripCurrent, gripTarget, Time.deltaTime * speed);
+            hAnimator.SetFloat(gripP, gripCurrent);
+        }
+
+        if(triggerCurrent != triggerTarget)
+        {
+            triggerCurrent = Mathf.MoveTowards(triggerCurrent, triggerTarget, Time.deltaTime * speed);
+            hAnimator.SetFloat(triggerP, triggerCurrent);
+        }
     }
 }
